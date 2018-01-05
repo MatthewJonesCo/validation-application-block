0976f6acd2544141a1087b0a9eec91e01894f007
Matthew Jones <Matthew_Jones@trimble.com>
2018-01-05 14:20:30 -0700

    Reference NonConfigurable.EnterpriseLibrary.Common

--- source/GlobalAssemblyInfo.cs
+++ source/GlobalAssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 #else
 [assembly: AssemblyConfiguration("Release")]
 #endif
-#endif
 [assembly: AssemblyCopyright("")]
+#endif
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
